c3372b1f82c9883963b8143dfddaa505a722cea6
Raúl Gutiérrez <167849538+RaulGutIzq@users.noreply.github.com>
2024-12-05 13:11:35 +0100

    Arreglada calculadora

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-const buttons = document.querySelectorAll("button");
-const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button"); //Selector todos los botones
+const display = document.querySelector(".display"); //Pantalla donde se envian los resultados
 
+
+//Añadir un listener a todos los botones
 buttons.forEach(function (button) {
   button.addEventListener("click", calculate);
 });
 
+//Añadir listener al teclado
 document.addEventListener("keydown", handleKeyboardInput);
 
+
+//Función para encapsular los botones
 function calculate(event) {
   const clickedButtonValue = event.target.value;
-  const regex = new RegExp("^([-+]?)([0-9]+)(?:([-+*/])((?:[-+])?[0-9]+))+$");
+  //Comprueba que el boton pulsado es el =
   if (clickedButtonValue === "=") {
     if (display.value !== "") {
       try {
-        if (!regex.test(display.value)) {
-          throw new Error();
-        }
         display.value = eval(display.value);
       } catch {
         display.value = "Error";
       }
     }
+    //Al pulsar el boton C se borra
   } else if (clickedButtonValue === "C") {
     display.value = "";
   } else {
@@ -30,9 +33,10 @@ function calculate(event) {
 
 function handleKeyboardInput(event) {
   const key = event.key;
+  //Comprueba que se ha pulsado un numero o un operador
   if (!isNaN(key) || ["+", "-", "*", "/"].includes(key)) {
     display.value += key;
-  } else if (key === "Enter") {
+    } else if (key === "Enter") { 
     if (display.value !== "") {
       try {
         display.value = eval(display.value);
@@ -40,9 +44,9 @@ function handleKeyboardInput(event) {
         display.value = "Error";
       }
     }
-  } else if (key === "Backspace") {
+  } else if (key === "Backspace") { //Si se presiona borrar, se borra el último número pulsado
     display.value = display.value.slice(0, -1);
-  } else if (key === "Delete") {
+  } else if (key === "Delete") {//Si se presiona suprimir, se borra todo el display
     display.value = "";
   }
 }
